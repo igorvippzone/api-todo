111d9f928eadbec23227e8f95741a031f1cf68d3
Агаларов Игорь <igorvippzone@gmail.com>
2023-04-07 14:43:47 +0300

    prettier check

--- models/Todo.js
+++ models/Todo.js
@@ -1,11 +1,9 @@
-const uidGenerator = require('node-unique-id-generator')
+const uidGenerator = require("node-unique-id-generator");
 
 class Todo {
-  constructor(title = '', desc='',id=uidGenerator.generateUniqueId()){
-    this.title = title,
-    this.desc = desc,
-    this.id = id
+  constructor(title = "", desc = "", id = uidGenerator.generateUniqueId()) {
+    (this.title = title), (this.desc = desc), (this.id = id);
   }
 }
 
-module.exports  =Todo
+module.exports = Todo;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const Todo = require('./Todo');
+const Todo = require("./Todo");
 
 module.exports = {
-    Todo,
+  Todo,
 };
